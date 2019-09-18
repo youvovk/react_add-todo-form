54158e3c6ca3a1bd3b8abb192efa13f38f4212ba
youvovk <wer3wollf@gmail.com>
2019-09-18 08:30:52 +0300

    Updates

---
+++ precache-manifest.c49f0641c29948546e7e4cd9a5652b5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3063852f49842c48497e168b88fd046",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "f09bde4a0f1b1825a0cf",
+    "url": "/react_add-todo-form/static/css/main.68ec95bb.chunk.css"
+  },
+  {
+    "revision": "2878e5a452f7bd7800c2",
+    "url": "/react_add-todo-form/static/js/2.10ee9465.chunk.js"
+  },
+  {
+    "revision": "f09bde4a0f1b1825a0cf",
+    "url": "/react_add-todo-form/static/js/main.fb813a63.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
